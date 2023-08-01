326e79e17315b7df6cf386ae83c1b9ad3f082a7d
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-01 22:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+let first= document.querySelector('#form1 input[name="fname"]');
+let last= document.querySelector('#form1 input[name="lname"]');
+	let fname=first.value;
+	let lname=last.value;
+alert(fname+' '+lname);
 }
